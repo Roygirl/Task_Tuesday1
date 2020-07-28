38052f8893df75f22ec2d4a0624e8b59bd7b71af
Roygirl <roygirl24@gmail.com>
2020-07-28 16:39:58 +0530

    Initial commit

---
+++ app/src/main/java/com/example/task_tuesday/MainActivity.java
@@ -0,0 +1,85 @@
+package com.example.task_tuesday;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+
+public class MainActivity extends AppCompatActivity {
+
+    ImageView imageView;
+    Button getcamera;
+    Bitmap bitmap;
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        bitmap =  (Bitmap)data.getExtras().get("data");
+        imageView.setImageBitmap(bitmap);
+
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        getcamera = findViewById(R.id.camera);
+
+        imageView = findViewById(R.id.imageView);
+
+        ActivityCompat.requestPermissions(this, new String[] { Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE }, 0);
+
+    }
+
+
+    public void get_camera(View view) {
+
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivityForResult(intent, 100);
+
+
+    }
+
+
+
+    public void save_image(View view) throws IOException {
+       // String path = Environment.getExternalStorageDirectory().toString();
+        OutputStream fOut = null;
+
+        File file = new File("/sdcard/xyz/"+"abc.jpg"); // the File to save , append increasing numeric counter to prevent files from getting overwritten.
+        fOut = new FileOutputStream(file);
+
+        Bitmap pictureBitmap = bitmap; // obtaining the Bitmap
+        pictureBitmap.compress(Bitmap.CompressFormat.JPEG, 85, fOut); // saving the Bitmap to a file compressed as a JPEG with 85% compression rate
+
+        fOut.close(); // do not forget to close the stream
+
+        MediaStore.Images.Media.insertImage(getContentResolver(),file.getAbsolutePath(),file.getName(),file.getName());
+
+        Toast.makeText(this, "Image Saved Successfully", Toast.LENGTH_SHORT).show();
+
+
+
+    }
+}
